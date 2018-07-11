8ca0578933bfcb3626e2c30d98bda95125085678
AndrewDongAndy <dongandrew99@gmail.com>
2018-07-11 18:42:52 -0400

    No good description; just solved more

--- Division 3/999D.cpp
+++ Division 3/999D.cpp
@@ -9,33 +9,48 @@ using namespace std;
 const int MAX = (int) 2e5;
 
 int N, M;
-int a[MAX];
+long long a[MAX];
 int cnt[MAX];
-int nextm[MAX];
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    fill(cnt, cnt + MAX, 0);
-    for (int i = 0; i < MAX; i++) {
-        nextm[i] = i;
-    }
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
 
     cin >> N >> M;
 
-    int ans = 0;
-    int avg = N / M;
+    set<int> s;
+    for (int i = 0; i < N; i++) {
+        cnt[i] = 0;
+        s.insert(i);
+    }
+
+    long long ans = 0;
     for (int i = 0; i < N; i++) {
         cin >> a[i];
-        int m = a[i] % M;
-        int ch = 0;
-        while (cnt[nextm[m]] + 1 > avg) {
-            ch++;
-            nextm[m] = (nextm[m] + 1) % M;
+
+        int nextm = -1;
+        if (a[i] % M > *s.rbegin()) {
+            nextm = *s.begin();
+        } else {
+            nextm = *s.lower_bound(a[i] % M);
         }
-        cnt[nextm[m]]++;
+        assert(nextm != -1);
+
+        int ch = nextm - a[i] % M;
+        if (ch < 0) ch += M;
+
         a[i] += ch;
         ans += ch;
+        cnt[a[i] % M]++;
+        if (cnt[a[i] % M] == N / M) s.erase(a[i] % M);
+    }
+
+    for (int i = 0; i < M; i++) {
+        assert(cnt[i] == N / M);
     }
 
     cout << ans << '\n';
