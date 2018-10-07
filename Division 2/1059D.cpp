d69ce720e6de21a9128669d4816b775230b93eae
AndrewDongAndy <dongandrew99@gmail.com>
2018-10-07 18:11:01 -0400

    Failed 1059D, but gained +58 rating from this last contest (Lyft Level 5)

--- Division 2/1059D.cpp
+++ Division 2/1059D.cpp
@@ -2,7 +2,9 @@
 // http://codeforces.com/problemset/problem/1059/D
 
 /*
+I HATE FLOATING-POINT ERRORS!!!
 
+Will fix this later.
 */
 
 
@@ -15,23 +17,23 @@ struct point {
 	int x, y;
 };
 
-const int INF = (int) 1e9;
-const int PRECISION = 100;
+const long double INF = 1e14;
+const int REPS = 200;
+const long double EPS = 1e-12;
 
 int N;
 vector<point> pts;
 
-bool works(double r) {
-	double y = r, x = INF;
-	for (int i = 0; i < N; i++) {
-		point &p = pts[i];
-		if (p.x > x) continue;
+bool works(long double r) {
+	long double y = r, x = INF;
+	for (point &p : pts) {
 		if (abs(y - p.y) > r) return false;
-		x = min(x, sqrt(pow(r, 2) - pow(y - p.y, 2)) + p.x);
+		if (p.x > x) continue;
+
+		long double s = sqrt(r - (y - p.y)) * sqrt(r + (y - p.y)) + p.x;
+		x = min(x, s);
 	}
-//	cout << "radius " << r << ": place circle at (" << x << ", " << y << ")\n";
-	for (int i = 0; i < N; i++) {
-		point &p = pts[i];
+	for (point &p : pts) {
 		if (hypot(x - p.x, y - p.y) > r) return false;
 	}
 	return true;
@@ -51,29 +53,26 @@ int main() {
 	cin >> N;
 	pts.resize(N);
 	int positive = 0, negative = 0, zero = 0;
-	int m = INF, M = -INF;
 	for (int i = 0; i < N; i++) {
 		point &p = pts[i];
 		cin >> p.x >> p.y;
 		if (p.y > 0) positive++;
 		else if (p.y < 0) negative++;
 		else zero++;
-		m = min(m, p.y);
-		M = max(M, p.y);
 	}
 	if (zero >= 2 || (positive > 0 && negative > 0)) {
 		cout << "-1\n";
 		return 0;
 	}
-	// binary search the answer
-	double a = (positive > 0 ? M / 2 : m / 2), b = INF;
 	if (positive == 0) {
 		for (point &p : pts) {
 			p.y = -p.y;
 		}
 	}
-	for (int _ = 0; _ < PRECISION; _++) {
-		double k = (a + b) / 2;
+	// binary search the answer
+	long double a = 0, b = INF;
+	for (int _ = 0; _ < REPS; _++) {
+		long double k = (a + b) / 2;
 		if (works(k)) {
 			b = k;
 		}
