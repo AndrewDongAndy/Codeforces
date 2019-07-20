27f244a99a12ed7d03f44df242c614dfd229ea85
AndrewDongAndy <dongandrew99@gmail.com>
2019-07-20 16:42:50 -0400

    added comment tips

--- 1178E.cpp
+++ 1178E.cpp
@@ -5,6 +5,24 @@
 Literally need to do more math.
 
 THIS SHOULD HAVE BEEN SO EASY WGAT :(((((
+
+
+Seriously though; tips:
+
+1. Use all of the constraints in the problem! e.g. if we only need to find a
+   working answer and they tell you it doesn't have to be optimal (as they do
+   here), then use that fact. Don't try to construct the optimal solution,
+   because if there were a way to do so, they probably would have asked for
+   that as the solution.
+2. If something is half of something else, try to find a way to use that fact
+   in the solution.
+3. If the general solution would be very hard, we probably have to use the
+   fact that it's more specific here. For example, I would think that finding
+   the longest palindromic subsequence in a general string would be pretty
+   hard; so we need to do something with the fact that the alphabet size is 3.
+4. Guess what information we can ignore. The fact that no two consecutive
+   letters were equal didn't seem to help, so it could be just to throw you off!
+   (This usually doesn't apply to math olympiad problems though.)
 */
 
 
