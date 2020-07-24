39a207a01b1928ea783ae934054694e3b46dea06
AndrewDongAndy <dongandrew99@gmail.com>
2020-07-24 19:08:38 -0400

    over 2200 now :D

---
+++ 1381A2.cpp
@@ -0,0 +1,29 @@
+// July 21, 2020
+// https://codeforces.com/contest/1381/problem/A2
+
+/*
+
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+#ifdef _DEBUG
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("debug_output.txt", "w", stderr);
+#endif
+  
+  
+  
+  
+  
+  return 0;
+}
